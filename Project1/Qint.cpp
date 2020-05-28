095ce2765abbc20ad5bf59046084e3a3904a11da
NgThanhHai <51964141+NgThanhHai@users.noreply.github.com>
2020-05-28 12:53:16 +0700

    Add files via upload

---
+++ Project1/Qint.cpp
@@ -0,0 +1,9 @@
+#include "Qint.h"
+
+Qint::Qint()
+{
+}
+void Qint::Nhap()
+{
+	
+}
---
+++ Project1/Qint.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class Qint
+{
+private:
+	int	_arrayBits[4];
+public:
+	Qint();
+	void Nhap();
+};
+
